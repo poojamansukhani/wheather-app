27bfe0e0e94b6fb209859f3d70ee11746cb205fb
Pooja Mansukhani <poojamansukhani94@gmail.com>
2022-05-19 23:58:27 +0530

    Dynamic data

--- custom.js
+++ custom.js
@@ -1,7 +1,20 @@
 const curDate = document.getElementById("date");
 let wheatherCon = document.getElementById("weathercon");
 
-const tempStatus = "clouds";
+const tempStatus = "{%tempStatus%}";
+if (tempStatus == "Sunny") {
+    weathercon.innerHTML =
+      "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
+  } else if (tempStatus == "Clouds") {
+    weathercon.innerHTML =
+      "<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>";
+  } else if (tempStatus == "Rainy") {
+    weathercon.innerHTML =
+      "<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
+  } else {
+    weathercon.innerHTML =
+      "<i class='fas  fa-cloud' style='color:#f1f2f6;'></i>";
+  }
 
 const getCurrentDay = () =>{
     var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
@@ -18,7 +31,8 @@ const getCurrentTime = () => {
     let period = "AM";
     if(hours > 11){
         period = "PM";
-        if(hours > 12) hours -= 12;
+        //1pm - 13 pm 13-12 = 1
+        if(hours > 12) hours -= 12; 
     }
     if(min < 10){
         min = "0" + min;
--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const fs = require("fs");
 const requests = require("requests");
 
 const homeFile = fs.readFileSync("index.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) => {
+    let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}", orgVal.name);
+    temperature = temperature.replace("{%country%}", orgVal.sys.country);
+    temperature = temperature.replace("{%tempStatus%}", orgVal.weather[0].main);
+  
+    return temperature;
+  };
 const server = http.createServer((req, res) => {
     if(req.url == "/"){
         requests("https://api.openweathermap.org/data/2.5/weather?q=indore&appid=c33284d363333228060ee203ae750b97")
@@ -11,11 +22,19 @@ const server = http.createServer((req, res) => {
             const objData = JSON.parse(chunk);
             //Pass object in array 
             const arrData  = [objData];
-            console.log(arrData);
+            const realTimeData = arrData
+            .map((val) => replaceVal(homeFile, val))
+            .join("");
+            res.write(realTimeData);
+            // console.log(realTimeData);
         })
-        .on("end", (err)=>{
-            if(err) return console.log("Connection closed due to error", err);
+        .on("end", (err) => {
+            if (err) return console.log("connection closed due to errors", err);
+            res.end();
         });
-    }
+    } else {
+        res.end("File not found");
+      }
 })
+
 server.listen(3000, "127.0.0.1");
